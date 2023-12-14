ba5efed297a706d06404435d6a2996bdad118c93
pritithakur <77279319+pritithakur@users.noreply.github.com>
2023-12-14 16:44:10 +0530

    Update Leapyear.java

--- Lab2/Leapyear.java
+++ Lab2/Leapyear.java
@@ -1,1 +1,20 @@
+import java.util.Scanner; 
+class Leapyear 
+{
+	public static void main(String[] args) 
+	{
+		int year;
+		System.out.println("Enter year: ");
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+		if((year % 4==0 && year%100!=0)|| year%400==0)
+		{
+			System.out.println("It is leap year!!");
+		}
+		else
+		{
+			System.out.println("It is not leap year!!");
+		}
+	}
+}
 
