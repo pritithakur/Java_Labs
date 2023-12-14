94f7bd3a032d716ae794f85604293ed3f0495c9f
pritithakur <77279319+pritithakur@users.noreply.github.com>
2023-12-14 16:41:46 +0530

    Update Maximum.java

--- Lab1/Maximum.java
+++ Lab1/Maximum.java
@@ -1,1 +1,22 @@
+import java.util.Scanner; 
+class Maximum 
+{
 
+	public static void main(String[] args) 
+	{
+		int num1 , num2;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		num1= input.nextInt();
+		System.out.println("Enter 2nd number: ");
+		num2 = input.nextInt();
+		if(num1>num2)
+		{
+			System.out.println(num1);
+		}
+		else
+		{
+			System.out.println(num2);
+		}
+	}
+}
